f3955967766d8d04e6aa767e199ed0e866a5bfb7
Dominik Jastrzębski <dominik@98elements.com>
2019-11-20 13:04:46 +0100

    Initial commit

---
+++ src/main/java/com/_98elements/gitversion/GitVersionDemoApplication.java
@@ -0,0 +1,13 @@
+package com._98elements.gitversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitVersionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitVersionDemoApplication.class, args);
+    }
+
+}
